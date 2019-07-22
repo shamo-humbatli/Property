1f160168d415bb1be2380179d9cafa203a2f65e9
Shamo Humbatli <51967485+shamo-humbatli@users.noreply.github.com>
2019-07-22 04:51:28 -0700

    Add files via upload

---
+++ Property/PropertiesBox.cs
@@ -0,0 +1,31 @@
+﻿/**
+ * <summary>author: Shamo Humbatli</summary>
+ */
+
+using System.Collections.Generic;
+
+namespace Property
+{
+    public class PropertiesBox
+    {
+        private Dictionary<string, string> properties;
+
+        public PropertiesBox()
+        {
+
+        }
+
+        public void AddProperty(string propertyKey, string propertyValue)
+        {
+            if(properties == null)
+            {
+                properties = new Dictionary<string, string>();
+            }
+
+            properties.Add(propertyKey, propertyValue);
+        }
+
+
+        public Dictionary<string, string> Properties { get => properties; set => properties = value; }
+    }
+}
---
+++ Property/PropertyBuilder.cs
@@ -0,0 +1,36 @@
+﻿/**
+ * <summary>author: Shamo Humbatli</summary>
+ */
+
+using System.IO;
+
+namespace Property
+{
+    public class PropertyBuilder
+    {
+        public PropertyBuilder()
+        {
+
+        }
+
+        public PropertiesBox LoadPropertiesFromFile(string filePath)
+        {
+            if (!File.Exists(filePath))
+            {
+                throw new FileNotFoundException();
+            }
+
+            string[] props = File.ReadAllLines(filePath);
+            PropertiesBox pb = new PropertiesBox();
+
+            foreach (string prop in props)
+            {
+                string key = prop.Split('=')[0].Trim();
+                string value = prop.Split('=')[1].Trim();
+
+                pb.AddProperty(key, value);
+            }
+            return pb;
+        }
+    }
+}
